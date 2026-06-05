18e4e1c29215a6f7ec945aa1db32af64e091ff2c
Amita Singh <amita3.ab@gmail.com>
2026-06-05 13:34:18 +0530

    MongoDB config and setup

---
+++ server/config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+require("dotenv").config({
+    path: path.join(__dirname, ".env"),
+    override: true,
+});
+
+module.exports = {
+    PORT: process.env.PORT || 3001,
+    MONGODB_URI: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017",
+    MONGODB_DB_NAME: process.env.MONGODB_DB_NAME || "nyayasahayak",
+};
