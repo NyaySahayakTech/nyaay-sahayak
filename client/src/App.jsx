631357960382374357dcf1684970d20795f018dd
Amita Singh <amita3.ab@gmail.com>
2026-06-29 08:09:43 +0530

    Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
 import Home from './pages/Home'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      {/* Hum aage chalkar yahan /login, /analyze routes add karenge */}
-    </Routes>
+    <div className="min-h-screen bg-gray-50">
+      <Navbar /> 
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
   )
 }
 
